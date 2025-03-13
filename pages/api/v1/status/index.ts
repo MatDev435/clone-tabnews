92beb274922637c82b5a1ebd4c54916521629e92
MatDev435 <matheuspsalmazo2@gmail.com>
2025-03-13 19:48:25 -0300

    Adiciona os arquivos database.ts e .env

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,8 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import database from "../../../../infra/database";
 
-export default function status(
+export default async function status(
   request: NextApiRequest,
   response: NextApiResponse,
 ) {
+  const result = await database.query("SELECT 1 + 1;");
+
   response.status(200).json({ message: "Hello, World!" });
 }
