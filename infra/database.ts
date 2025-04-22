2c0a3f3acd642e5fe5963e3c5fa2ce369894e27d
MatDev435 <matheuspsalmazo2@gmail.com>
2025-04-22 09:53:59 -0300

    Makes database client.connect() more rebust to errors

--- infra/database.ts
+++ infra/database.ts
@@ -9,14 +9,16 @@ async function query(queryObject: string | QueryConfig<any[]>) {
     password: process.env.POSTGRES_PASSWORD,
   });
 
-  await client.connect();
-
   try {
+    await client.connect();
+
     const result = await client.query(queryObject);
 
     return result;
   } catch (error) {
     console.error(error);
+
+    throw error;
   } finally {
     await client.end();
   }
